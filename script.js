49cce8771b4e5812e6d2ce566d5d8ed44776b8f6
unknown <AliceLiu1218>
2023-01-29 01:48:44 -0500

    make api call

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var cityInputEl = document.querySelector("#cityInput")
 var searchBtn = document.querySelector("#searchButton");
 var searchRecord = document.querySelector("#historyContainer");
-//console.log(cityInputEl.value);
 
 var buttonClickHandler = function (event) {
     event.preventDefault();
@@ -9,26 +8,40 @@ var buttonClickHandler = function (event) {
     var city = cityInputEl.value.trim();
     console.log(cityInputEl.value);
     if (city) {
-        //getWeather(city);
+        getGeoCoordination(city);
 
+       // Append search history
         var cityEl = document.createElement("button");
-        cityEl.classList = "btn btn-secondary";
+        cityEl.classList = "btn btn-secondary btn-city";
         cityEl.innerHTML = city;
         searchRecord.appendChild(cityEl);
-
-        //<button type="button" class="btn btn-secondary">Secondary</button>
-        //<a href="#" class="list-group-item list-group-item-action list-group-item-primary">A simple primary list group item</a>
-        //repoContainerEl.textContent = '';
-       // cityInputEl.value = '';
+        
+        //clear input value
+        cityInputEl.value = '';
        
       } else {
         alert('Please enter a city name');
       }
 }
 
-var getWeather = function (city) {
-   // var apiURL = 
+var getGeoCoordination = function (city) {
+  // get lat,lon
+  var lanlonURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid=0d990e0dff1f939ceefa0f13e3b23ec6';
+  //var apiURL = api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+  fetch(lanlonURL)
+    .then (function (reponse){
+      console.log(reponse);
+      if (response.ok){
+        response.json().then(function (data){
+          console.log(data);
+        })
+      }
+    else {
+      alert('Error: ' + response.statusText);
+    }
+    })
 }
 
 
-searchBtn.addEventListener('submit', buttonClickHandler);
+searchBtn.addEventListener('click', buttonClickHandler);
